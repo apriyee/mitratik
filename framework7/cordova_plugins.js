17f30281fa766b6d76e9425d662e96efe4d20a12
Apri Suharto <apriyee@gmail.com>
2023-04-11 20:16:33 +0800

    add font-awesome framework7
    Penambahan font awesome dan framework7 untuk template mobile device

---
+++ framework7/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function (require, exports, module) {
+    module.exports = [
+        {
+            "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+            "id": "cordova-plugin-statusbar.statusbar",
+            "pluginId": "cordova-plugin-statusbar",
+            "clobbers": [
+                "window.StatusBar"
+            ]
+        }
+    ];
+    module.exports.metadata =
+            {
+                "cordova-plugin-whitelist": "1.2.2",
+                "cordova-plugin-console": "1.0.7",
+                "cordova-plugin-statusbar": "1.0.1"
+            }
+});
